5331ab1483fcb2edaed9bde0b80f80231154550c
Nguyen Hai Dang <ndang3399@gmail.com>
2022-04-10 17:13:04 +0700

    Fixing leak secretkey

--- TodoApplication/settings.py
+++ TodoApplication/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h*=*&&*wsv@-3#l!@08%2!042yxdv%)55^#lpur1(&uboj2+ha'
+SECRET_KEY = '<YOUR-SECRET-KEY>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
