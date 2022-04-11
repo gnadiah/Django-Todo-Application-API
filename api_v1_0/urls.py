633b898f34800cebcb6ef1923c880192c6ec0e45
Nguyen Hai Dang <ndang3399@gmail.com>
2022-04-11 18:15:29 +0700

    Add new features

--- api_v1_0/urls.py
+++ api_v1_0/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('getTask/', views.get_task, name='get_task'),
+    path('getTask/', views.get_task, name='getTask'),
+    path('getSpecificTask/<str:task_id>/',
+         views.get_specific_task, name='getSpecificTask'),
     path('createTask/', views.create_task, name='createTask'),
     path('editTask/<str:task_id>/', views.edit_task, name='editTask'),
     path('deleteTask/<str:task_id>/', views.delete_task, name='deleteTask'),
